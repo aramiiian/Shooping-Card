aba7ec33393e93723ec8b6539c5a5061f856014a
aramiiian <haykazaramyan439@gmail.com>
2023-02-21 17:39:02 +0400

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,98 @@
+export const shopData = [
+  {
+    productImage: "",
+    productName: "Cropped Stay Groovy off white",
+    productPrice: `$10.90`,
+    prodSale: `or 9 x$1.21`,
+  },
+  {
+    productImage: "",
+    productName: "Basic Cactus White T-shirt",
+    productPrice: `$13.25`,
+    prodSale: `or 3 x$4.42`,
+  },
+  {
+    productImage: "",
+    productName: "Skater Black Sweatshirt",
+    productPrice: `$25.90`,
+    prodSale: `or 12 x$2.16`,
+  },
+  {
+    productImage: "",
+    productName: "Black Tule Oversized",
+    productPrice: `$29.45`,
+    prodSale: `or 5 x$5.89`,
+  },
+  {
+    productImage: "",
+    productName: "Black Batman T-shirt",
+    productPrice: `$10.90`,
+    prodSale: `or 9 x$1.21`,
+  },
+  {
+    productImage: "",
+    productName: "Blue T-Shirt",
+    productPrice: `$9.00`,
+    prodSale: `or 3 x$3.00`,
+  },
+  {
+    productImage: "",
+    productName: "Loose Black T-shirt",
+    productPrice: `$14.00`,
+    prodSale: `or 5 x$2.80`,
+  },
+  {
+    productImage: "",
+    productName: "Ringer Hall Pass",
+    productPrice: `$10.90`,
+    prodSale: `or 9 x$1.21`,
+  },
+  {
+    productImage: "",
+    productName: "Grey T-shirt",
+    productPrice: `$14.90`,
+    prodSale: `or 7 x$2.13`,
+  },
+  {
+    productImage: "",
+    productName: "Black T-shirt with white stripes",
+    productPrice: `$14.90`,
+    prodSale: `or 7 x$2.13`,
+  },
+  {
+    productImage: "",
+    productName: "Turtles Ninja T-shirt",
+    productPrice: `$10.90`,
+    prodSale: `or 9 x$1.21`,
+  },
+  {
+    productImage: "",
+    productName: "Slim black T-shirt",
+    productPrice: `$49.90`,
+    prodSale: `or 9 x$5.44`,
+  },
+  {
+    productImage: "",
+    productName: "Blue Sweatshirt",
+    productPrice: `$22.50`,
+    prodSale: `or 4 x$5.63`,
+  },
+  {
+    productImage: "",
+    productName: "White T-shirt Gucci",
+    productPrice: `$18.70`,
+    prodSale: `or 4 x$4.67`,
+  },
+  {
+    productImage: "",
+    productName: "Tropical Wine T-shirt",
+    productPrice: `$134.90`,
+    prodSale: `or 5 x$26.98`,
+  },
+  {
+    productImage: "",
+    productName: "Marine Blue T-shirt",
+    productPrice: `$49.00`,
+    prodSale: `or 9 x$5.44`,
+  },
+];
